1f99acd2f8af4380c9d3f3ecbea64b4a774456b3
Esteban Samuel Reyes <estebansamuel.reyes@gmail.com>
2024-02-23 08:54:06 -0600

    delete console log

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import IrdrResearch from './components/irdrResearch';
 import './App.css'
 function App() {
-   console.log('test');
   return (
     <>
       <section className='main-composer'>
