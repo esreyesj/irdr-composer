e3d2e74016270221742a62adef13480df7827a0d
Esteban Samuel Reyes <estebansamuel.reyes@gmail.com>
2024-02-23 08:48:22 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import IrdrResearch from './components/irdrResearch';
 import './App.css'
 function App() {
+  console.log('App');
   return (
     <>
       <section className='main-composer'>
